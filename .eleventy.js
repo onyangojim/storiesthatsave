750b4a1f71638fe529d26e781a2678c3d229d4d8
onyangojim <onyango.j@gmail.com>
2025-07-26 07:40:44 +0300

    Update .eleventy.js
    module.exports = function (eleventyConfig) {
      eleventyConfig.addPassthroughCopy("static");
    
      return {
        dir: {
          input: "src",
          output: "_site",
          includes: "_includes",
          data: "_data",
        },
      };
    };

--- .eleventy.js
+++ .eleventy.js
@@ -1,1 +1,13 @@
 
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("static");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data",
+    },
+  };
+};
