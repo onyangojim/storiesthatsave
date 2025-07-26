11e59140a4b58d58073b66d024e2328476607a50
onyangojim <onyango.j@gmail.com>
2025-07-26 16:35:55 +0300

    Update .eleventy.js
    module.exports = function (eleventyConfig) {
      eleventyConfig.addPassthroughCopy("admin");
      eleventyConfig.addPassthroughCopy("static");
    
      eleventyConfig.addCollection("stories", function (collection) {
        return collection.getFilteredByGlob("content/stories/*.md");
      });
    
      return {
        dir: {
          input: "src",
          includes: "_includes",
          data: "_data",
          output: "_site",
        },
      };
    };

--- .eleventy.js
+++ .eleventy.js
@@ -1,1 +1,17 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("static");
 
+  eleventyConfig.addCollection("stories", function (collection) {
+    return collection.getFilteredByGlob("content/stories/*.md");
+  });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+  };
+};
